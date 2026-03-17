e521e4182814e1c48f70228e0dda2bb01283d5c4
xmakaco <mak.sarzotti@gmail.com>
2026-03-17 21:18:04 +0100

    Task 2

--- oob_basics/language_histograms.py
+++ oob_basics/language_histograms.py
@@ -5,5 +5,15 @@ def histogram_characters_ger(input_sentence):
         ger_characters_dict[i] = char_count
     print(ger_characters_dict)
 
-input_sentence = "Die Studierenden klönen"
-histogram_characters_ger(input_sentence)
+def histogram_characters_eng(input_sentence):
+    chars_conversion = [("ö", "oe"), ("ü", "ue"), ("ä", "ae"), ("ß", "ss")]
+    eng_characters_dict = {}
+    def convert_chars(input_sentence):
+        for k, v in chars_conversion:
+            converted_sentence = input_sentence.replace(k, v)
+            return converted_sentence
+    input_sentence = convert_chars(input_sentence)
+    for i in input_sentence.casefold():
+        char_count = input_sentence.casefold().count(i)
+        eng_characters_dict[i] = char_count
+    print(eng_characters_dict)
